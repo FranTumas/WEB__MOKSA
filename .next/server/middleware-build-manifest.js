84adb6f48b58654e6c6d6dc12e0df0a3b23c3bfc
FranTumas <tumasfranc@gmail.com>
2025-12-29 18:45:46 -0300

    DOu

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -8,11 +8,11 @@ globalThis.__BUILD_MANIFEST = {
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
-    "static/chunks/e97bb96cb3c593c6.js",
+    "static/chunks/f3a8bf824c7d1efc.js",
     "static/chunks/01787aef0bc207ac.js",
     "static/chunks/8296bf97416a5ebf.js",
     "static/chunks/834800a6b130715c.js",
-    "static/chunks/turbopack-6fa43c8e87121863.js"
+    "static/chunks/turbopack-434277c071e1ce1a.js"
   ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
